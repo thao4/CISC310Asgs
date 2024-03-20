610b2a49b948d4f6b2d06fd1fb5be99e32a3f753
thao4 <162147199+thao4@users.noreply.github.com>
2024-03-20 09:03:36 -0500

    Added starter methods to assignment 5

--- Assignment 5/Assignment 5.java
+++ Assignment 5/Assignment 5.java
@@ -1,5 +1,20 @@
-public Class Paging {
-    public static void main(String[] args){
-        
-    }
+public Class Assignment {
+
+}
+
+abstract Class Paging {
+    abstract void page(int[] pages);
+
+}
+
+Class FIFO extends Paging {
+
+}
+
+Class LRU extends Paging {
+
+}
+
+Class Optimal extends Paging {
+
 }
